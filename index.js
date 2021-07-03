9549b0edf31bcf277458dda9e2eb33da2f441303
Jarad Solomon <jaradsolomon@gmail.com>
2021-07-03 01:16:34 -0500

    swap image and fetch new artwork

--- index.js
+++ index.js
@@ -2,28 +2,39 @@ document.addEventListener("DOMContentLoaded", () => {
 allTheStuff()
 })
 
+
 function allTheStuff(){
 
-console.log('starting!')
-
+    
 const URL = 'https://api.artic.edu/api/v1/artworks/129884'
+const newButton = document.querySelector('#newMatch')
+const artwork = document.querySelector('#art')
+
 
 function getImage(){
-fetch(URL)
-.then(response => response.json())
-//.then ( data => console.log(data))
-}
-
-function updateImage(){
-    getImage()
+    fetch(URL)
+    .then(response => response.json())
     .then(data => {
-        
+        return`${data.config.iiif_url}`+'/'+`${data.data.image_id}`+'/full/843,/0/default.jpg'
     })
-}
+    .then(link => {
+        artwork.src = link
+    })
+   
+    }
 
 
 
 
+
+newButton.addEventListener('click', () => {
+
+getImage()
+
+//let newArtLink = 'https://api.artic.edu/api/v1/artworks/' + `${Math.floor(Math.random() * 200000)}`
+//artwork.innerHTML = `src=${newArtLink} alt='An Artwork' class="rotate90" id='art'`
+    
+})
 
 
 
